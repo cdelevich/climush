79f3793a044562bd57430d404ae6f9b155b225c0
carolyndelevich <cdelevic@uoregon.edu>
2024-04-06 19:48:16 -0700

    package v0.0.2, remove package imports from __init__.py

--- bioinformatics-pipeline/climush_py-package/climush/__init__.py
+++ bioinformatics-pipeline/climush_py-package/climush/__init__.py
@@ -1,5 +1,5 @@
-from .config import config
-from .bioinfo import bioinfo
-from .constants import constants
-from .utilities import utilities
-from .viz import viz
+# from .config import config
+# from .bioinfo import bioinfo
+# from .constants import constants
+# from .utilities import utilities
+# from .viz import viz
