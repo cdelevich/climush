8eba1199786bc3cfdb74607cd61605412cd767e8
carolyndelevich <cdelevic@uoregon.edu>
2024-04-08 12:42:38 -0700

    fixed curl output path

--- bioinformatics-pipeline/climush_get-started.sh
+++ bioinformatics-pipeline/climush_get-started.sh
@@ -5,7 +5,7 @@ echo "PWD=$(pwd)" >> .env
 
 # pull the compose.yaml file from GitHub to set up Docker image
 #wget -O compose.yaml https://github.com/cdelevich/climush.git#main:/docker-containers/bioinformatics/deploy/compose.yaml
-curl -o compose.yaml https://github.com/cdelevich/climush.git#main:/docker-containers/bioinformatics/deploy/compose.yaml
+curl -o . https://github.com/cdelevich/climush.git#main:/docker-containers/bioinformatics/deploy/compose.yaml
 
 # create an empty sequence directory
 mkdir sequences
@@ -13,7 +13,7 @@ mkdir sequences
 # create a config directory and add configuration file into it
 mkdir config
 #wget -O /config/climush-bioinfo_pipeline-settings.toml https://github.com/cdelevich/climush.git#main:/bioinformatics-pipeline/config/climush-bioinfo_pipeline-settings.toml
-curl -o /config/climush-bioinfo_pipeline-settings.toml https://github.com/cdelevich/climush.git#main:/bioinformatics-pipeline/config/climush-bioinfo_pipeline-settings.toml
+curl -o /config https://github.com/cdelevich/climush.git#main:/bioinformatics-pipeline/config/climush-bioinfo_pipeline-settings.toml
 
 # run Docker container from Docker image via compose.yaml
 docker compose run climush-bioinformatics
