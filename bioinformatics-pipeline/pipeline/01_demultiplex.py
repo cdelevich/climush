2bf3edb307a27ca8879b12a44087fa0d8caedabd
carolyndelevich <cdelevic@uoregon.edu>
2024-05-05 12:08:39 -0700

    add command line options

--- bioinformatics-pipeline/pipeline/01_demultiplex.py
+++ bioinformatics-pipeline/pipeline/01_demultiplex.py
@@ -8,6 +8,60 @@ from climush.utilities import *
 from climush.bioinfo import demultiplex
 
 settings = get_settings(fpm)
+run_name = settings['run_details']['run_name']
+
+parser = argparse.ArgumentParser(prog=Path(__file__).stem,
+                                 description='Demultiplex PacBio reads using PacBio\'s Lima demultiplexing '
+                                             'tool.',
+                                 epilog='')
+
+# input directory containing the files to demultiplex
+parser.add_argument('-i', '--input',
+                    default=fpm['sequences']['demux'],
+                    help='The path to a directory containing sequencing files to demultiplex. If nothing provided, '
+                         'will default to the location that is expected in the Docker container\'s native file '
+                         'structure, detailed in pipeline/mapping.py.')
+
+# output directory to send the demultiplexed reads to
+parser.add_argument('-o', '--output',
+                    default=fpm['pipeline-output']['demultiplexed'],
+                    help='The path to a directory containing sequencing files to demultiplex. If nothing provided, '
+                         'will default to the location that is expected in the Docker container\'s native file '
+                         'structure, detailed in pipeline/mapping.py.')
+
+# to run quality filtering and merging separately, for time restriction
+parser.add_argument('--merge-from', nargs='?',
+                    default=None,
+                    help='If running merging separately from quality filtering, provide the path to the '
+                         'directory containing the quality-filtered paired sequence files to merge.')
+
+# if option provided, uses this minimum read length filter instead of configuration settings
+parser.add_argument('--min-length', nargs='?',
+                    default=settings['quality_filtering'],
+                    help='The minimum read length permitted to pass the quality filter. Using this command line '
+                         'argument will override the settings from your configuration file.')
+
+# if option provided, uses this maximum read length filter instead of configuration settings
+parser.add_argument('--max-length', nargs='?',
+                    default=settings['quality_filtering'],
+                    help='The maximum read length permitted to pass the quality filter. Using this command line '
+                         'argument will override the settings from your configuration file.')
+
+# if option provided, uses this maximum expected error filter instead of configuration settings
+parser.add_argument('--max-error', nargs='?',
+                    default=settings['quality_filtering'],
+                    help='The maximum expected error allowed to pass the quality filter. Using this command line '
+                         'argument will override the settings from your configuration file.')
+
+
+args = vars(parser.parse_args())
+
+# parse default or CL arguments
+# if an input path is provided, convert to a Path object
+if isinstance(args['input'], str):
+    input_path = Path(args['input'])
+else:
+    input_path = args['input']
 
 # set variable that will only switch to True if files requiring demultiplexing are detected
 files_demuxed = False
@@ -15,9 +69,10 @@ files_demuxed = False
 #####################
 # ILLUMINA ##########
 #####################
+platform = 'illumina'
 
 # check if there are Illumina reads that need to be demultiplexed
-is_input, illumina_files = check_for_input(fpm['sequences']['demux'], seq_platform='illumina')
+is_input, illumina_files = check_for_input(fpm['sequences']['demux'], seq_platform=platform)
 
 if is_input:
     msg = f'WARNING. Currently, there is no script that can demultiplex Illumina reads. You can continue with the ' \
@@ -30,6 +85,7 @@ else:
 #####################
 # PACBIO ############
 #####################
+platform = 'pacbio'
 
 # check if there are PacBio reads that need to be demultiplexed
 is_input, pacbio_files = check_for_input(fpm['sequences']['demux'], seq_platform='\d{4}')
@@ -49,9 +105,10 @@ else:
 #####################
 # SANGER ############
 #####################
+platform = 'sanger'
 
 # check if there are Sanger reads that need to be demultiplexed
-is_input, sanger_files = check_for_input(fpm['sequences']['demux'], seq_platform='sanger')
+is_input, sanger_files = check_for_input(fpm['sequences']['demux'], seq_platform=platform)
 
 if is_input:
     msg = f'WARNING. Currently, there is no script that can demultiplex Sanger reads. You can continue with the ' \
@@ -65,5 +122,5 @@ else:
 
 # when all are demultiplexed (if possible), continue to the next script
 if not files_demuxed:  # print if no demultiplexing was carried out
-    print(f'No sequencing files were detected in the /sequences directory that require demultiplexing.\n')
+    print(f'No sequencing files were detected in the {input_path} that require demultiplexing.\n')
 continue_to_next(__file__, settings)
