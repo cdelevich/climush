3835f88bc853511711a2368bfa65d32fe2b1d4d4
carolyndelevich <cdelevic@uoregon.edu>
2024-07-30 18:14:23 -0700

    updated CL default input to work for both PacBio and Illumina

--- bioinformatics-pipeline/pipeline/05_dereplicate-01.py
+++ bioinformatics-pipeline/pipeline/05_dereplicate-01.py
@@ -15,7 +15,7 @@ parser = argparse.ArgumentParser(prog=Path(__file__).stem,
 
 # input directory containing the files to dereplicate
 parser.add_argument('-i', '--input',
-                    default=fpm['pipeline-output']['primers-trimmed'] / f'trim_{run_name}',
+                    default=None,  # depends on previous pipeline steps, settings
                     type=pathlib.PosixPath,
                     help='The path to a directory containing sequencing files to dereplicate. If nothing provided, '
                          'will default to the location that is expected in the Docker container\'s native file '
@@ -23,25 +23,53 @@ parser.add_argument('-i', '--input',
 
 args = vars(parser.parse_args())
 
+## REMOVE AFTER TESTING
+args = {'input': None}
+
 #####################
 # ILLUMINA ##########
 #####################
-# platform = 'illumina'
-#
+platform = 'illumina'
+
+# where should the input be located for Illumina reads?
+if args['input'] is None:
+
+    # which folder from quality-filtered should be used (i.e., merged or unmerged)
+    if settings['quality_filtering'][platform]['merge_reads']:  # if reads were merged
+        dir_name = f'{MERGED_PREFIX}_{run_name}'
+    else:
+        dir_name = f'{QUALFILT_PREFIX}'
+
+    # make full path with dir
+    input_path = fpm['pipeline-output']['quality-filtered'] / dir_name
+
+else:
+
+    input_path = args['input']
+
+
 # check that there are Illumina reads to dereplicate
-# last_output = [dir for dir in fpm['pipeline-output']['quality-filtered'].glob('*') if re.search(f'^{QUALFILT_PREFIX}', dir.stem, re.I)][0]
-# is_input, illumina_files = check_for_input(last_output)
-#
-# if is_input:
-#     dereplicate(input_files=illumina_files, derep_step=1, platform='illumina', file_map=fpm)
-# else:
-#     pass
+is_input, illumina_files = check_for_input(file_dir=input_path, seq_platform=platform)
+
+if is_input:
+    print(f'Dereplicating {len(illumina_files)} {platform} reads...\n')
+
+    dereplicate(input_files=illumina_files, derep_step=1, platform='illumina', file_map=fpm)
+else:
+    pass
+
 #####################
 # PACBIO ############
 #####################
 platform = 'pacbio'
 
-is_input, pacbio_files = check_for_input(file_dir=args['input'], seq_platform=platform)
+# where should the input be located for PacBio reads?
+if args['input'] is None:
+    input_path = fpm['pipeline-output']['primers-trimmed'] / f'trim_{run_name}'
+else:
+    input_path = Path(args['input'])
+
+is_input, pacbio_files = check_for_input(file_dir=input_path, seq_platform=platform)
 
 if is_input:
     dereplicate(input_files=pacbio_files, derep_step=1, platform=platform, file_map=fpm)
